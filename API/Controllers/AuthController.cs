502eb11c391814e2d2d9977ec7bd41d671f588fc
kwesi-selorm <adorkor.jeffery@gmail.com>
2023-09-21 15:38:22 +0200

    controller fns to register and log in

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +17,38 @@ namespace API.Controllers
         public AuthController(UserManager<IdentityUser> userManager)
         {
             this.userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(UserDTO user)
+        {
+            IdentityResult? result = await userManager.CreateAsync(
+                new IdentityUser() { UserName = user.UserName, Email = user.Email },
+                user.Password
+            );
+            if (!result.Succeeded)
+            {
+                List<string>? errors = result.Errors
+                    .Select(e => e.Description.TrimEnd('.'))
+                    .ToList();
+                string detail = string.Join(", ", errors);
+                return Problem(detail: detail, statusCode: StatusCodes.Status400BadRequest);
+            }
+            return Created("", new UserDTO() { UserName = user.UserName, Email = user.Email });
+        }
+
+        [HttpGet("{userName}")]
+        public async Task<IActionResult> GetUser(string userName)
+        {
+            IdentityUser? userRecord = await userManager.FindByNameAsync(userName);
+            if (userRecord == null)
+            {
+                return Problem(
+                    detail: $"A user with username {userName} was not found",
+                    statusCode: StatusCodes.Status404NotFound
+                );
+            }
+            return Ok(new { userRecord.UserName, userRecord.Email });
         }
     }
 }
