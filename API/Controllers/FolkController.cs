7b74d0e366bdfb43c8d7ae10f980b1071c870f3d
kwesi-selorm <adorkor.jeffery@gmail.com>
2023-09-25 10:45:54 +0200

    add service function for img compression

--- API/Controllers/FolkController.cs
+++ API/Controllers/FolkController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace API.Controllers
 {
--- API/Services/ImageService.cs
+++ API/Services/ImageService.cs
@@ -1,3 +1,6 @@
+using System.IO.Compression;
+using System;
+
 namespace API.Services
 {
     public class ImageService
@@ -17,7 +20,25 @@ namespace API.Services
             if (bytes.Length == 0)
                 return null;
             string base64String = Convert.ToBase64String(bytes);
+            // string compressedBase64String = await CompressBase64String(base64String);
             return base64String;
+        }
+
+        public async Task<string> CompressBase64String(string base64String)
+        {
+            string compressedBase64String;
+            byte[] bytes = Convert.FromBase64String(base64String);
+
+            using (MemoryStream ms = new())
+            {
+                using (GZipStream zip = new(ms, CompressionMode.Compress))
+                {
+                    await zip.WriteAsync(bytes);
+                }
+                byte[] compressedBytes = ms.ToArray();
+                compressedBase64String = Convert.ToBase64String(compressedBytes);
+            }
+            return compressedBase64String;
         }
     }
 }
