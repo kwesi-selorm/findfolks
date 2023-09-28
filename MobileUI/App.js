ef9fff6ee8d744db1097811bc4cec79bf135917b
Jeffery Adorkor <jead@v-p-s.com>
2023-09-28 12:48:26 +0200

    Move custom fonts from App to styles.ts

--- MobileUI/App.js
+++ MobileUI/App.js
@@ -6,13 +6,8 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import DiscoverScreen from './src/screens/discover'
 import CommunityScreen from './src/screens/community'
 import { useFonts } from 'expo-font'
+import { customFonts } from './src/styles'
 // expo install expo-font
-
-let customFonts = {
-  'Bricolage Grotesque': require('./assets/fonts/BricolageGrotesque-Regular.ttf'),
-  'Bricolage Grotesque Bold': require('./assets/fonts/BricolageGrotesque-Bold.ttf'),
-  'Bricolage Grotesque Medium': require('./assets/fonts/BricolageGrotesque-Medium.ttf')
-}
 
 function getScreenOptions() {
   return ({ route, navigation }) => ({
---
+++ MobileUI/src/styles.ts
@@ -0,0 +1,19 @@
+let customFonts = {
+  'Bricolage Grotesque Light': require('../assets/fonts/BricolageGrotesque-Light.ttf'),
+  'Bricolage Grotesque': require('../assets/fonts/BricolageGrotesque-Regular.ttf'),
+  'Bricolage Grotesque Medium': require('../assets/fonts/BricolageGrotesque-Medium.ttf'),
+  'Bricolage Grotesque Bold': require('../assets/fonts/BricolageGrotesque-Bold.ttf'),
+  'Bricolage Grotesque SemiBold': require('../assets/fonts/BricolageGrotesque-SemiBold.ttf'),
+  'Bricolage Grotesque ExtraBold': require('../assets/fonts/BricolageGrotesque-ExtraBold.ttf')
+}
+
+const fontFamily = {
+  light: 'Bricolage Grotesque Light',
+  regular: 'Bricolage Grotesque',
+  medium: 'Bricolage Grotesque Medium',
+  bold: 'Bricolage Grotesque Bold',
+  semiBold: 'Bricolage Grotesque SemiBold',
+  extraBold: 'Bricolage Grotesque ExtraBold'
+}
+
+export { fontFamily, customFonts }
