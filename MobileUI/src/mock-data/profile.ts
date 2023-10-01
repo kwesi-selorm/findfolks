b72d9a94c1a44e4d6661b6a0b80a78e7678a9e47
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-10-01 15:02:51 +0200

    Add profile mock data

---
+++ MobileUI/src/mock-data/profile.ts
@@ -0,0 +1,17 @@
+import { Profile } from '../@types'
+import { connections } from './connections'
+import { torProfilePhoto } from './bas64strings'
+
+const profile: Profile = {
+  id: 1,
+  name: 'Tor',
+  homeCountry: 'Norway',
+  homeCityOrTown: 'Bergen',
+  countryOfResidence: 'Norway',
+  cityOrTownOfResidence: 'Stavanger',
+  profilePhoto: torProfilePhoto,
+  bio: 'Hi there',
+  preferredContactMethod: 0,
+  contactInfo: '',
+  connections: connections
+}
