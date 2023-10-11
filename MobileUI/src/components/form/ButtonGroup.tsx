0759e49ea99e31eb84a5310d9f431b6a3be59c09
Jeffery Adorkor <jead@v-p-s.com>
2023-10-11 13:16:05 +0200

    Create ButtonGroup component

---
+++ MobileUI/src/components/form/ButtonGroup.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, View } from 'react-native'
+import { FC, ReactNode } from 'react'
+
+type ButtonsGroupProps = {
+  children: ReactNode
+  direction?: 'row' | 'column'
+}
+
+const ButtonGroup: FC<ButtonsGroupProps> = ({ children, direction }) => {
+  const styles = StyleSheet.create({
+    row: {
+      display: 'flex',
+      flexDirection: direction ?? 'row',
+      gap: 10,
+      justifyContent: 'center',
+      width: '50%',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginVertical: 40
+    }
+  })
+
+  return <View style={styles.row}>{children}</View>
+}
+
+export default ButtonGroup
